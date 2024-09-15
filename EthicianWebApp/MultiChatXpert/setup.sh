8685a56dfc299bd5a3f03e09fc4cc5bf6f86d4a9
BigHatHenley <rhhenley26@gmail.com>
2024-09-15 00:38:12 -0400

    setup.sh fix

--- EthicianWebApp/MultiChatXpert/setup.sh
+++ EthicianWebApp/MultiChatXpert/setup.sh
@@ -3,12 +3,15 @@ if [ "$(uname)" == "Darwin" ]; then
     # macOS
     echo "Detected macOS. Installing portaudio..."
     brew install portaudio
+    brew link portaudio
+    pip install pyaudio
 elif [ -f /etc/debian_version ]; then
     # Debian-based Linux (e.g., Ubuntu)
     echo "Detected Debian-based Linux. Installing portaudio19-dev..."
     sudo apt-get update
     sudo apt-get install -y portaudio19-dev
     sudo apt-get install -y python-pyaudio
+    pip install --global-option='build_ext' --global-option='-I/usr/local/include' --global-option='-L/usr/local/lib' pyaudio
 elif [ -f /etc/redhat-release ]; then
     # Red Hat-based Linux (e.g., Fedora, CentOS)
     echo "Detected Red Hat-based Linux. Installing portaudio-devel..."
