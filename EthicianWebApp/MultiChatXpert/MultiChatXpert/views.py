3cd3bf798d98e02b82df70476eb73edfc8b00467
BigHatHenley <rhhenley26@gmail.com>
2024-10-16 13:12:28 -0400

    views update

--- EthicianWebApp/MultiChatXpert/MultiChatXpert/views.py
+++ EthicianWebApp/MultiChatXpert/MultiChatXpert/views.py
@@ -10,7 +10,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from serializers import UserAccountSerializer
+from .serializers import UserAccountSerializer
 from django.http import HttpResponse
 from .mongodb_access import add_user, get_user, add_conversation
 
