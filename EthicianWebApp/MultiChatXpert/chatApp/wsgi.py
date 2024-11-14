c3f56ad016504378a409eb4ad8703d1db4b734e7
BigHatHenley <rhhenley26@gmail.com>
2024-11-14 02:51:39 -0500

    wsgi fix

--- EthicianWebApp/MultiChatXpert/chatApp/wsgi.py
+++ EthicianWebApp/MultiChatXpert/chatApp/wsgi.py
@@ -8,8 +8,12 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(project_root)
+sys.path.append(os.path.join(project_root, 'chatApp'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatApp.settings')
 
